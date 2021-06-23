739138bbef8a7ed162456fafbf7a9d5808b4f1d2
malagege <s9714020@gmail.com>
2021-06-23 22:26:23 +0800

    mutation 6

--- src/main.js
+++ src/main.js
@@ -5,14 +5,18 @@ import { SOME_MUTATION } from './mutation-types'
 
 // 創建一個新的 store 實例
 const store = createStore({
-  state: { count:0 },
+  state: {
+      name: '張三',
+      number: 0,
+  },
   mutations: {
-    // 我們可以使用 ES2015 風格的計算屬性命名功能來使用一個常量作為函數名
-    [SOME_MUTATION] (state) {
-      state.count += 1
-    }
+      setNumberIsWhat(state, payload) {
+          setTimeout(() => {
+            state.number = payload.number;
+          }, 1000);
+      },
   }
-})
+});
 
   
 // 將 store 實例作為插件安裝
@@ -20,8 +24,11 @@ const store = createStore({
 createApp(App).use(store).mount('#app')
 
 
-store.commit({
-  type: SOME_MUTATION
+console.log(`舊值：${store.state.number}`);
+
+store.commit('setNumberIsWhat',{
+  number: 20
 })
 
-console.log('store.state.count:' + store.state.count)
+// await store.dispatch('setNum');
+console.log(`新值：${store.state.number}`);
